dca54a1ef2736989f0d3bfd6a4cf937974c34524
Chris <ckeenan@starry.com>
2020-12-06 13:52:50 -0500

    Bumped version to 0.1.3

--- buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.presentation"
     const val owner = "chrynan"
     const val repoName = "presentation"
-    const val versionName = "0.1.2"
-    const val versionCode = 3
-    const val versionDescription = "Pre-release 0.1.2"
+    const val versionName = "0.1.3"
+    const val versionCode = 4
+    const val versionDescription = "Pre-release 0.1.3"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/presentation.git"
     const val androidCompileSdkVersion = 29
