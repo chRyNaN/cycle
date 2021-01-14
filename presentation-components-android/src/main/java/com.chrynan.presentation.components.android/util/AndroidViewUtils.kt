f32f085d3e354dba6c65ac87f430610ec5b1ad58
Chris <ckeenan@starry.com>
2021-01-14 20:44:35 +0700

    Added snackbar function to components-android module

--- presentation-components-android/src/main/java/com.chrynan.presentation.components.android/util/AndroidViewUtils.kt
+++ presentation-components-android/src/main/java/com.chrynan.presentation.components.android/util/AndroidViewUtils.kt
@@ -7,7 +7,11 @@ import android.graphics.Outline
 import android.view.View
 import android.view.ViewOutlineProvider
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import com.chrynan.presentation.components.model.Notification
 import com.chrynan.presentation.components.model.Shape
+import com.chrynan.presentation.components.viewmodel.NotificationViewModel
+import com.google.android.material.snackbar.Snackbar
 
 fun View.setBackgroundShape(shape: Shape) {
     val outline = object : ViewOutlineProvider() {
@@ -75,4 +79,47 @@ fun View.showKeyboard() {
 fun View.hideKeyboard() {
     val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun snackbarOf(
+    view: View?,
+    viewModel: NotificationViewModel
+): Snackbar? {
+    if (view == null) return null
+
+    val duration = when (viewModel.notification.length) {
+        Notification.Length.SHORT -> Snackbar.LENGTH_SHORT
+        Notification.Length.LONG -> Snackbar.LENGTH_LONG
+        Notification.Length.INDEFINITE -> Snackbar.LENGTH_INDEFINITE
+    }
+
+    return Snackbar.make(view, viewModel.notification.message, duration).apply {
+        viewModel.notification.action?.let { action ->
+            setAction(action.title) { action.block() }
+        }
+
+        when (viewModel.notification.type) {
+            Notification.Type.ERROR -> setTextColor(viewModel.colors.textErrorColor.colorInt)
+            Notification.Type.MESSAGE -> setTextColor(viewModel.colors.textMessageColor.colorInt)
+            Notification.Type.SUCCESS -> setTextColor(viewModel.colors.textSuccessColor.colorInt)
+            Notification.Type.LOADING -> setTextColor(viewModel.colors.textLoadingColor.colorInt)
+            Notification.Type.OTHER -> setTextColor(viewModel.colors.textOtherColor.colorInt)
+        }
+
+        when (viewModel.notification.type) {
+            Notification.Type.ERROR -> setActionTextColor(viewModel.colors.textErrorColor.colorInt)
+            Notification.Type.MESSAGE -> setActionTextColor(viewModel.colors.textMessageColor.colorInt)
+            Notification.Type.SUCCESS -> setActionTextColor(viewModel.colors.textSuccessColor.colorInt)
+            Notification.Type.LOADING -> setActionTextColor(viewModel.colors.textLoadingColor.colorInt)
+            Notification.Type.OTHER -> setActionTextColor(viewModel.colors.textOtherColor.colorInt)
+        }
+
+        when (viewModel.notification.type) {
+            Notification.Type.ERROR -> setBackgroundTint(viewModel.colors.backgroundErrorColor.colorInt)
+            Notification.Type.MESSAGE -> setBackgroundTint(viewModel.colors.backgroundMessageColor.colorInt)
+            Notification.Type.SUCCESS -> setBackgroundTint(viewModel.colors.backgroundSuccessColor.colorInt)
+            Notification.Type.LOADING -> setBackgroundTint(viewModel.colors.backgroundLoadingColor.colorInt)
+            Notification.Type.OTHER -> setBackgroundTint(viewModel.colors.backgroundOtherColor.colorInt)
+        }
+    }
 }
