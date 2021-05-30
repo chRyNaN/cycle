4978a167f415390667af4bc4201e8ab611318a33
Chris <ckeenan@starry.com>
2021-05-30 15:20:49 -0500

    Created BasePresenterFactory

---
+++ presentation-core/src/commonMain/kotlin/com.chrynan.presentation/BasePresenterFactory.kt
@@ -0,0 +1,11 @@
+@file:Suppress("unused")
+
+package com.chrynan.presentation
+
+/**
+ * An extension on a [PresenterFactory] that returns a [BasePresenter] instead of just a [Presenter].
+ */
+fun interface BasePresenterFactory<I : Intent, S : State, C : Change> : PresenterFactory<I, S, C> {
+
+    override fun invoke(view: View<I, S>): BasePresenter<I, S, C>
+}
