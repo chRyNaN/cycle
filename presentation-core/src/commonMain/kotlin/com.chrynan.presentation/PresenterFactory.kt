0843de823d89cb143d8041aa22d96ac5ed1e5b09
Chris <ckeenan@starry.com>
2021-05-30 15:18:51 -0500

    Created PresenterFactory

---
+++ presentation-core/src/commonMain/kotlin/com.chrynan.presentation/PresenterFactory.kt
@@ -0,0 +1,13 @@
+@file:Suppress("unused")
+
+package com.chrynan.presentation
+
+/**
+ * A component that can create a [Presenter]. This allows lazy instantiation of a [Presenter] which
+ * may be required to wait for the [View] to be created first, then passing that into the [invoke]
+ * function to create the [Presenter] that will be bound to that [View].
+ */
+fun interface PresenterFactory<I : Intent, S : State, C : Change> {
+
+    operator fun invoke(view: View<I, S>): Presenter<I, S, C>
+}
