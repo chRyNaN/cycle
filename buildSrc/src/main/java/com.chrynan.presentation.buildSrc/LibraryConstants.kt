59c9263faec1637336f275ad056f49dde5136034
Christopher Keenan <ckeenan@starry.com>
2021-10-03 21:46:39 -0500

    Updated version to 0.3.2 (8)

--- buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.presentation"
     const val owner = "chrynan"
     const val repoName = "presentation"
-    const val versionName = "0.3.1"
-    const val versionCode = 7
-    const val versionDescription = "Release 0.3.1 (7)"
+    const val versionName = "0.3.2"
+    const val versionCode = 8
+    const val versionDescription = "Release 0.3.2 (8)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/presentation.git"
 
