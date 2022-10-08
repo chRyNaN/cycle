f37caf2e44f8813db5edfd6f2c99eab903908ce0
Chris <ckeenan@starry.com>
2022-10-08 09:37:30 -0500

    Bumped version to 0.9.0 (21)

--- buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.presentation.buildSrc/LibraryConstants.kt
@@ -5,8 +5,8 @@ object LibraryConstants {
     const val group = "com.chrynan.presentation"
     const val owner = "chrynan"
     const val repoName = "presentation"
-    const val versionName = "0.8.0"
-    const val versionCode = 20
+    const val versionName = "0.9.0"
+    const val versionCode = 21
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/presentation.git"
