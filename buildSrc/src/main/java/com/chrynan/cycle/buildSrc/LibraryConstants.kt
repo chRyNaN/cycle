9dcb397f876685a9c77f39f5e923bac30bd5312c
Chris Keenan <chrynan@pm.me>
2023-04-14 08:53:05 -0500

    Bumped version to 0.11.0 (23)

--- buildSrc/src/main/java/com/chrynan/cycle/buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com/chrynan/cycle/buildSrc/LibraryConstants.kt
@@ -5,8 +5,8 @@ object LibraryConstants {
     const val group = "com.chrynan.cycle"
     const val owner = "chrynan"
     const val repoName = "cycle"
-    const val versionName = "0.10.0"
-    const val versionCode = 22
+    const val versionName = "0.11.0"
+    const val versionCode = 23
     const val versionDescription = "Release $versionName ($versionCode)"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/cycle.git"
